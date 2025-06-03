393aa4ec6fcb45677d9baa03e60b91fc40159b9c
ahnshy <ahnshy@naver.com>
2025-06-03 17:42:59 +0900

    <feat> : i18N English language resources added

---
+++ locales/en.ts
@@ -0,0 +1,64 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default defineI18nLocale((locale) => {
+  return {
+    seo: {
+      homes: {
+        title: 'Home',
+        menuName: 'Home',
+        description: '',
+      },
+      blogs: {
+        title: 'Blog',
+        menuName: 'Blog',
+        description: '',
+      },
+      aboutMe: {
+        title: 'About',
+        menuName: 'About',
+        description: '',
+      },
+      donations: {
+        title: 'Donations',
+        description: '',
+      },
+      links: {
+        title: 'Links',
+        description: '',
+      },
+    },
+    me: {
+      name: 'ahnshy',
+      position: 'Software Engineer',
+      description: ``,
+    },
+    labels: {
+      categories: 'Categories',
+      locales: {
+        en: 'English',
+        ko: 'Korean',
+      },
+      orders: {
+        recent: 'Recent',
+        old: 'Old',
+        Asc: 'Ascending',
+        Desc: 'Descending',
+      },
+    },
+    categories: {
+      programming: 'Programming',
+    },
+    tags: {
+      algorithm: 'Algorithm',
+    },
+    welcome: 'Welcome',
+    pages: {
+      homes: {
+        popularTags: 'Popular Tags',
+      },
+      donations: {
+        thanks1: '',
+        thanks2: '',
+      },
+    },
+  }
+})
