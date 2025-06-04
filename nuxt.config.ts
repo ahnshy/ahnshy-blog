f21179d1943ac920e48792405f5b3077abc50c78
ahnshy <ahnshy@naver.com>
2025-06-04 21:43:00 +0900

    <feat> : Adding nuxt config settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,99 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-05-15',
-  devtools: { enabled: true },
-
   modules: [
+    "@nuxt/content",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+    "@nuxtjs/i18n",
+    "@nuxt/image",
     '@nuxt/fonts',
     '@nuxt/icon',
-    '@nuxt/image',
     '@nuxt/scripts',
     '@nuxt/ui',
     '@nuxt/test-utils',
     '@nuxt/eslint',
     '@nuxt/content'
-  ]
+  ],
+  compatibilityDate: '2025-05-15',
+  devtools: { enabled: true },
+  app: {
+    head: {
+      meta: [
+        {
+          name: 'google-site-verification',
+          content: process.env.NUXT_GOOGLE_SITE_VERIFICATION || ''
+        },
+      ],
+      script: [
+        {
+          type: 'text/javascript',
+          src: 'https://cdnjs.buymeacoffee.com/1.0.0/button.prod.min.js',
+          'data-name': 'bmc-button',
+          'data-slug': 'youngjinkwak',
+          'data-color': '#FFDD00',
+          'data-emoji': '',
+          'data-font': 'Cookie',
+          'data-text': 'Buy me a coffee',
+          'data-outline-color': '#000000',
+          'data-font-color': '#000000',
+          'data-coffee-color': '#ffffff',
+        }
+      ]
+    }
+  },
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        name: 'English',
+        file: 'en.ts'
+      },
+      {
+        code: 'ko',
+        name: '한국어',
+        file: 'ko.ts',
+      },
+    ],
+    strategy: 'prefix_and_default',
+    lazy: true,
+    langDir: 'locales',
+    defaultLocale: 'en',
+    // vueI18n: './i18n.config.ts' // if you are using custom path, default
+  },
+  content: {
+    //highlight: {
+      // theme: {
+      //   default: 'github-light',
+      //   // light: 'github-light',
+      //   dark: 'github-dark',
+      // },
+      //theme: 'github-dark',
+    //},
+    markdown: {
+      toc: {
+        depth: 5, searchDepth: 5
+      },
+      remarkPlugins: ['remark-reading-time'],
+    },
+  },
+  googleFonts: {
+    display: 'swap',
+    download: true,
+    families: {
+      Roboto: true,
+      'Noto Sans': true,
+    }
+  },
+  site: {
+    indexable: true,
+    url: process.env.NUXT_PUBLIC_SITE_URL,
+  },
+  sitemap: {
+    // https://nuxtseo.com/sitemap/integrations/content
+    strictNuxtContentPaths: true
+  },
+  gtag: {
+    id: process.env.GTAG_ID,
+  }
+  // devtools: { enabled: true },
 })
